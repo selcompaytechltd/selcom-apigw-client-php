6f3e23137dea8d2cb906d6a1008dd584ded096d8
gradoMabachi <126318320+gradoMabachi@users.noreply.github.com>
2023-02-25 11:38:34 +0300

    add initial files

---
+++ Readme.md
@@ -0,0 +1,86 @@
+<h1 align="center">SELCOM API ACCESS CLIENT</h1>
+
+<p align='center'>
+
+<img src="https://img.shields.io/badge/php-%3D%3E7-blue">
+
+</p >
+
+## Description
+This is a library containing functions that aid in the accessing of selcom api. IT is made up pf 4 functions.
+computeHeader 
+
+## Installation
+
+composer require something/something
+
+## Usage
+```php
+require_once __DIR__ .'/vendor/autoload.php';
+// use modeule
+use apigwClient\Client ;
+//iniiatialize new Client with values of the base url, api key and api secret
+$client = new Client($baseUrl, $apiKey, $apiSecret);
+// computeHeader takes an array containing data to bes submitted
+// computeHeader returns an array with values for the following header fields: 
+//  Authorization, Timestamp, Digest, Signed-Fields
+$client->computeHeader( $arrayData):
+
+// postFuct takesa path relative to baseUrl. array containing data of query  
+// It performs a POST request of the submitted data to the destniation url generatingg the header internally
+// IT returns a json containing the response to the request
+$client->postFunc($path, $arrayData)
+
+// getFuct takes a path relative to baseUrl. array containing data of query 
+// It performs a GET request adding the query to the  url and generatingg the header internally
+// IT returns a json containing the response to the request
+$client->getFunc($path, $arrayData)
+
+// deletetFuct takes a path relative to baseUrl. array containing data of query 
+// It performs a DELETE request adding the query to the  url and generatingg the header internally
+// IT returns a json containing the response to the request
+$client->deleteFunc($path, $arrayData)
+```
+
+## Example
+
+```php
+require_once __DIR__ .'/vendor/autoload.php';
+
+// 
+use apigwClient\Client ;
+
+# initalize a new apiAccess instace with values of the base url, api key and api secret
+
+$apiKey = '202cb962ac59075b964b07152d234b70';
+$apiSecret = '81dc9bdb52d04dc20036dbd8313ed055';
+$baseUrl = "http://example.com/v1/checkout/";
+
+
+$client = new Client($baseUrl, $apiKey, $apiSecret);
+
+
+//order data
+$orderArray = array(
+"vendor"=>"VENDORTILL",
+"order_id"=>"1218d5Qb",
+"buyer_email"=> "john@example.com",
+"buyer_name"=> "John Joh",
+"buyer_phone": "255682555555",
+"amount"=>  8000,
+"currency"=>"TZS",
+"buyer_remarks"=>"None",
+"merchant_remarks"=>"None",
+"no_of_items"=>  1
+
+)
+
+// path relatiive to base url
+$orderPath = "create-order-minimal"
+// crate new order
+
+$response = $client->postFunc($orderPath,$orderArray);
+
+echo json_encode($response);
+
+```
---
+++ src/apigwClient.php
@@ -0,0 +1,107 @@
+<?php
+namespace apigwClient;
+class Client{
+    protected String $baseUrl;
+    protected String $apiKey;
+    protected String $apiSecret; 
+
+    public function __construct($baseUrl, $apiKey, $apiSecret){
+        $this->baseUrl = $baseUrl;
+        $this->apiKey = $apiKey;
+        $this->apiSecret = $apiSecret;
+    }
+    public function computeHeader( $arrayData){
+        date_default_timezone_set('Africa/Dar_es_Salaam');
+        $authToken = "SELCOM" .' '.base64_encode($this->apiKey);
+
+        $signedFields  =implode(',', array_keys($arrayData));
+        $fieldOrder = explode(',', $signedFields);
+        $timestamp = date('c');
+        $data = "timestamp=$timestamp";
+        foreach ($fieldOrder as $key) {
+        $data .= "&$key=".strval($arrayData[$key]);
+        }
+        $digest = base64_encode(hash_hmac('sha256', $data, $this->apiSecret, true));
+        return [$authToken, $timestamp, $digest, $signedFields ];
+
+    }
+
+    public function postFunc($path, $arrayData){
+        [$authToken, $timestamp, $digest, $signedFields ] = $this->computeHeader($arrayData);
+        $urls = $this->baseUrl . $path;
+        $jsonData = json_encode($arrayData);
+        $headers = array(
+        "Content-type: application/json", 
+        "Authorization: $authToken",
+        "Digest-Method: HS256",
+        "Digest: $digest",
+        "Timestamp: $timestamp",
+        "Signed-Fields: $signedFields",
+        );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $urls);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch,CURLOPT_TIMEOUT,90);
+        curl_setopt($ch,CURLOPT_VERBOSE,true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $resp = json_decode($result, true);
+        return $resp;
+        
+    }
+
+    public function getFunc($path, $arrayData){
+        [$authToken, $timestamp, $digest, $signedFields ] = $this->computeHeader($arrayData);
+        $urls = $this->baseUrl . $path . '?' . http_build_query($arrayData);
+        $headers = array(
+        "Content-type: application/json", 
+        "Authorization: $authToken",
+        "Digest-Method: HS256",
+        "Digest: $digest",
+        "Timestamp: $timestamp",
+        "Signed-Fields: $signedFields",
+        );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $urls);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch,CURLOPT_TIMEOUT,90);
+        curl_setopt($ch,CURLOPT_VERBOSE,true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $resp = json_decode($result, true);
+        return $resp;
+        
+    }
+
+    public function deleteFunc($path, $arrayData){
+        [$authToken, $timestamp, $digest, $signedFields ] = $this->computeHeader($arrayData);
+        $urls = $this->baseUrl . $path . '?' . http_build_query($arrayData);
+        $headers = array(
+        "Content-type: application/json", 
+        "Authorization: $authToken",
+        "Digest-Method: HS256",
+        "Digest: $digest",
+        "Timestamp: $timestamp",
+        "Signed-Fields: $signedFields",
+        );
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $urls);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch,CURLOPT_TIMEOUT,90);
+        curl_setopt($ch,CURLOPT_VERBOSE,true);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $resp = json_decode($result, true);
+        return $resp;
+        
+    }
+
+
+}
+ ?>
